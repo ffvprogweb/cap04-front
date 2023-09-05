a017b8e4cf2ff72c0ca60cb7083e00e85e5f81e4
Edson Saraiva de Almeida <68782201+ffvprogweb@users.noreply.github.com>
2023-09-05 10:06:33 -0300

    Initial commit

---
+++ src/componentes/ConsultaCatalogo/index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+const ConsultaCatalogo = () => {
+  return (
+    <div className="Catalogo">
+      <h3> Consulta Catalogo </h3>
+    </div>
+  );
+};
+export default ConsultaCatalogo;
